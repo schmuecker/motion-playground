e45f1c3e46c632225d7a6021d341a769010ac529
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-09-21 20:38:49 +0200

    Add toast messages

--- src/pages/Toast/Toast.tsx
+++ src/pages/Toast/Toast.tsx
@@ -1,15 +1,39 @@
-import { useSpring, animated } from "@react-spring/web";
+import { useSpring, animated, useTransition } from "@react-spring/web";
 import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import { tw } from "twind";
 import Button from "../../components/Button";
 
+function Message({ visible }: { visible: Boolean }) {
+  const transitions = useTransition(visible, {
+    from: { opacity: 0, x: 20 },
+    enter: { opacity: 1, x: 0 },
+    leave: { opacity: 0, x: -20 },
+  });
+  return transitions(
+    (styles, item) =>
+      item && (
+        <animated.div
+          style={styles}
+          className={tw`bg-purple-400 px-2 py-1 rounded-xl`}
+        >
+          Message
+        </animated.div>
+      )
+  );
+}
+
 function Toast() {
-  const [on, show] = useState(false);
-  const toggle = () => show((o) => !o);
-  const styles = useSpring({ opacity: on ? 1 : 0, y: on ? 0 : -20 });
   return (
     <div>
-      <Button bg="purple" text="Show toast" onClick={toggle} />
-      <animated.div style={styles}>Toast</animated.div>
+      <Toaster />
+      <Button
+        bg="purple"
+        text="Show toast"
+        onClick={() => {
+          toast.custom((t) => <Message visible={t.visible} />);
+        }}
+      />
     </div>
   );
 }
