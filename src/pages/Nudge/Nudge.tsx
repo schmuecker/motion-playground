c51b0502e24f6c726a9c4a5b4082a3e2e32c0852
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-09-27 14:08:15 +0200

    add tailwindcss + prettier

--- src/pages/Nudge/Nudge.tsx
+++ src/pages/Nudge/Nudge.tsx
@@ -1,7 +1,6 @@
 import { animated, useTransition } from "@react-spring/web";
 import { toast, Toaster } from "react-hot-toast";
 import { useToaster } from "react-hot-toast";
-import { tw } from "twind";
 import Button from "../../components/Button";
 import Dot from "../../components/Dot";
 
@@ -17,20 +16,21 @@ function Message({ text, subtext, visible }: MessageProps) {
     enter: { opacity: 1, y: 0, scale: 1 },
     leave: { opacity: 0, y: 0, scale: 0.75 },
   });
+
   return transitions(
     (styles, item) =>
       item && (
         <animated.div
           style={styles}
-          className={tw`w-auto inline-block py-5 pl-6 pr-12 bg-black font-sans rounded-full`}
+          className="inline-block w-auto rounded-full bg-black py-5 pl-6  pr-12 font-sans"
         >
-          <div className={tw`flex items-center gap-5`}>
+          <div className={"flex items-center gap-5"}>
             <Dot w={45} h={45} color="#4ade80" border="#fff" borderWidth={1} />
-            <div className={tw`flex-grow-0`}>
-              <div className={tw`font-bold whitespace-nowrap flex-grow-0`}>
+            <div className={"flex-grow-0"}>
+              <div className={"flex-grow-0 whitespace-nowrap font-bold"}>
                 {text}
               </div>
-              <div className={tw`font-bold whitespace-nowrap opacity-60`}>
+              <div className={"whitespace-nowrap font-bold opacity-60"}>
                 {subtext}
               </div>
             </div>
@@ -45,7 +45,7 @@ function Nudge() {
   const hasToast = toasts.filter((t) => t.visible).length > 0;
 
   return (
-    <div className={tw`h-auto`}>
+    <div className={"h-auto"}>
       <Toaster toastOptions={{ duration: 2000 }} />
       <Button
         bg="purple"
